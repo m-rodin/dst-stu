72312fd04f9d63d6b51bd83135175e711cccedbf
Denis Bondarenko <bond.den@gmail.com>
2017-03-13 11:14:37 -0700

    itl rds

--- src/install-redis.sh
+++ src/install-redis.sh
@@ -3,13 +3,21 @@
 cDir="$PWD"
 
 v='3.2.8'
-f="redis-$v.tar.gz"
+n="redis-$v"
+f="$n.tar.gz"
 
 cd ~
 
-curl -L -O "http://download.redis.io/releases/$f"
-tar -xf "$f"
-unlink "./$f"
+# curl -L -O "http://download.redis.io/releases/$f"
+# tar -xf "$f"
+# unlink "./$f"
 
-cd "$cDir"
+cd "$n"
+
+# make && \
+make test && sudo make install
+
+# cd "$cDir"
+
+
 
